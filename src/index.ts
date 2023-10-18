4be26485048764fbf38934eb44123e5e566deb54
Anamaria Miranda <anny13miranda@gmail.com>
2023-10-18 18:01:56 -0500

    Added routes to index ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
+import 'dotenv/config'
 
-dotenv.config();
+import gamesRoutes from './routers/games.routes'
+import locationsRoutes from './routers/locations.routes'
+import teamsRoutes from './routers/teams.routes'
 
-const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 8080
+const ENV = process.env.NODE_ENV || 'production'
 
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
-});
+const app: Express = express()
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:8000`);
-});
+app.use(cors())
+
+app.use(express.json())
+
+app.use('/games', gamesRoutes)
+app.use('/locations', locationsRoutes)
+app.use('/teams', teamsRoutes)
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} in ${ENV} environment`)
+})
+
+export { app as default, server }
